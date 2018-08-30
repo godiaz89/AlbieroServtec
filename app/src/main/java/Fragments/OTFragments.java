b37908c639d5bc554301500517aa1f7dd1ac5aa2
Gonzalo Benjamin Diaz Uria <gonzaloit1989@gmail.com>
2018-08-30 13:14:21 -0300

    30 ago 13.14

--- app/src/main/java/Fragments/OTFragments.java
+++ app/src/main/java/Fragments/OTFragments.java
@@ -3,16 +3,29 @@ package Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.albiero.albierost.R;
+import net.albiero.albierost.RecyclerAdapter;
+
+import java.util.List;
+
+import testClasses.Ots;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OTFragments extends Fragment {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lm;
+    private static RecyclerView rv;
+    private static List<Ots> ots;
+
+
 
 
     public OTFragments() {
@@ -24,6 +37,30 @@ public class OTFragments extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_ot, container, false);
+        rv=(RecyclerView)view.findViewById(R.id.otRecycler);
+        lm= new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(lm);
+
+        ots.add(new Ots("AL-5259/0","Gonzalo Diaz","Falla bateria", "Problema de bateria en abonado, el señor pindonga llamo quejandose" ));
+        ots.add(new Ots("AL-10/0","AREVALO SRL","GPRS No Comunica", "Los pibes de arevalo estan calientes porque no van los tecnicos"));
+        ots.add(new Ots("AL-9840/0","BOSCH TUCUMAN", "OTROS", "Rodolfo rompe el pingo quiere que veamos la bosch"));
+        ots.add(new Ots("AL-5259/0","Gonzalo Diaz","Falla bateria", "Problema de bateria en abonado, el señor pindonga llamo quejandose" ));
+        ots.add(new Ots("AL-10/0","AREVALO SRL","GPRS No Comunica", "Los pibes de arevalo estan calientes porque no van los tecnicos"));
+        ots.add(new Ots("AL-9840/0","BOSCH TUCUMAN", "OTROS", "Rodolfo rompe el pingo quiere que veamos la bosch"));
+        ots.add(new Ots("AL-5259/0","Gonzalo Diaz","Falla bateria", "Problema de bateria en abonado, el señor pindonga llamo quejandose" ));
+        ots.add(new Ots("AL-10/0","AREVALO SRL","GPRS No Comunica", "Los pibes de arevalo estan calientes porque no van los tecnicos"));
+        ots.add(new Ots("AL-9840/0","BOSCH TUCUMAN", "OTROS", "Rodolfo rompe el pingo quiere que veamos la bosch"));
+        ots.add(new Ots("AL-5259/0","Gonzalo Diaz","Falla bateria", "Problema de bateria en abonado, el señor pindonga llamo quejandose" ));
+        ots.add(new Ots("AL-10/0","AREVALO SRL","GPRS No Comunica", "Los pibes de arevalo estan calientes porque no van los tecnicos"));
+        ots.add(new Ots("AL-9840/0","BOSCH TUCUMAN", "OTROS", "Rodolfo rompe el pingo quiere que veamos la bosch"));
+        ots.add(new Ots("AL-5259/0","Gonzalo Diaz","Falla bateria", "Problema de bateria en abonado, el señor pindonga llamo quejandose" ));
+        ots.add(new Ots("AL-10/0","AREVALO SRL","GPRS No Comunica", "Los pibes de arevalo estan calientes porque no van los tecnicos"));
+        ots.add(new Ots("AL-9840/0","BOSCH TUCUMAN", "OTROS", "Rodolfo rompe el pingo quiere que veamos la bosch"));
+
+        adapter = new RecyclerAdapter(ots);
+        rv.setAdapter(adapter);
+
+
 
         // Inflate the layout for this fragment
         return view;
